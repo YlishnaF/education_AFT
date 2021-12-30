3771cf1386b201a4c919c179b55073637e4d38f7
YlishnaF <63332058+YlishnaF@users.noreply.github.com>
2021-12-30 16:08:16 +0500

    Update Company.java

--- src/lession3/Company.java
+++ src/lession3/Company.java
@@ -1,13 +1,6 @@
-import com.google.gson.JsonDeserializationContext;
-import com.google.gson.JsonDeserializer;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParseException;
 
-import java.lang.reflect.Type;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.Locale;
 
 public class Company {
     private int id;
