a38c8638292d84328f58fecb0f6f813f2387af50
YlishnaF <63332058+YlishnaF@users.noreply.github.com>
2022-01-03 20:15:17 +0500

    Update Currency.java

--- src/lession3/Currency.java
+++ src/lession3/Currency.java
@@ -1,9 +1,9 @@
+import com.google.gson.annotations.SerializedName;
 public enum Currency {
+    @SerializedName("RUB")
     RUB,
+    @SerializedName("USD")
     USD,
+    @SerializedName("EU")
     EU;
-
-    Currency() {
-    }
-
 }
